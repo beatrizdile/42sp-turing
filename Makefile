714e0ce19069f8ce026e461ec656f6d40d77b96d
Beatriz Dile <bedos-sa@student.42sp.org.br>
2025-06-29 17:06:24 -0300

    feat: parsing json machine

--- Makefile
+++ Makefile
@@ -1,18 +1,22 @@
-SRC = ft_turing.ml
-OUTPUT = ft_turing
+SRC_DIR = src
 BUILD_DIR = build
-OBJ = $(BUILD_DIR)/ft_turing.cmx
+OUTPUT = ft_turing
+
+SRC_FILES = $(SRC_DIR)/parsing_json.ml $(SRC_DIR)/ft_turing.ml
+OBJ_FILES = $(BUILD_DIR)/parsing_json.cmx $(BUILD_DIR)/ft_turing.cmx
 
 all: $(OUTPUT)
 
 $(BUILD_DIR):
 	mkdir -p $(BUILD_DIR)
 
-$(OBJ): $(SRC) | $(BUILD_DIR)
-	opam exec -- ocamlfind ocamlopt -package yojson -c -o $(OBJ) $(SRC)
+# Compile each .ml file into .cmx and .cmi
+$(BUILD_DIR)/%.cmx: $(SRC_DIR)/%.ml | $(BUILD_DIR)
+	opam exec -- ocamlfind ocamlopt -package yojson -I $(BUILD_DIR) -c -o $@ $<
 
-$(OUTPUT): $(OBJ)
-	opam exec -- ocamlfind ocamlopt -package yojson -linkpkg -o $(OUTPUT) $(OBJ)
+# Link all object files to make the executable
+$(OUTPUT): $(OBJ_FILES)
+	opam exec -- ocamlfind ocamlopt -package yojson -linkpkg -I $(BUILD_DIR) -o $@ $(OBJ_FILES)
 
 clean:
 	rm -f $(BUILD_DIR)/*.cmx $(BUILD_DIR)/*.o $(BUILD_DIR)/*.cmi
---
+++ src/parsing_json.ml
@@ -0,0 +1,71 @@
+open Map
+open Yojson.Safe.Util
+
+type transitions = {
+  read : string;
+  to_state : string;
+  write : string;
+  action : string;
+}
+
+type turing_machine = {
+  name : string;
+  alphabet : string list;
+  blank : string;
+  states : string list;
+  initial : string;
+  finals : string list;
+  transitions : (string, transitions list) Hashtbl.t;
+}
+
+let transitions_of_json json =
+  {
+    read = json |> member "read" |> to_string;
+    to_state = json |> member "to_state" |> to_string;
+    write = json |> member "write" |> to_string;
+    action = json |> member "action" |> to_string;
+  }
+
+let turing_machine_from_json json =
+  let transitions_tbl = Hashtbl.create 10 in
+  let transitions_json = json |> member "transitions" |> to_assoc in
+  List.iter (fun (state, trans_list_json) ->
+    let trans_list = trans_list_json |> to_list |> List.map transitions_of_json in
+    Hashtbl.add transitions_tbl state trans_list
+  ) transitions_json;
+  {
+    name = json |> member "name" |> to_string;
+    alphabet = json |> member "alphabet" |> to_list |> List.map to_string;
+    blank = json |> member "blank" |> to_string;
+    states = json |> member "states" |> to_list |> List.map to_string;
+    initial = json |> member "initial" |> to_string;
+    finals = json |> member "finals" |> to_list |> List.map to_string;
+    transitions = transitions_tbl;
+  }
+
+let print_transition t =
+  Printf.printf "    { read = %s; to_state = %s; write = %s; action = %s }\n"
+    t.read t.to_state t.write t.action
+
+let print_turing_machine tm =
+  let border = String.make 80 '*' in
+  let center_text text =
+    let len = String.length text in
+    let padding = (76 - len) / 2 in
+    Printf.sprintf "*%s%s%s*" (String.make padding ' ') text (String.make (76 - len - padding) ' ')
+  in
+  Printf.printf "%s\n" border;
+  Printf.printf "%s\n" (center_text "");
+  Printf.printf "%s\n" (center_text tm.name);
+  Printf.printf "%s\n" (center_text "");
+  Printf.printf "%s\n" border;
+  Printf.printf "Alphabet: [ %s ]\n" (String.concat ", " tm.alphabet);
+  Printf.printf "States  : [ %s ]\n" (String.concat ", " tm.states);
+  Printf.printf "Initial : %s\n" tm.initial;
+  Printf.printf "Finals  : [ %s ]\n" (String.concat ", " tm.finals);
+  Hashtbl.iter (fun state transitions ->
+    List.iter (fun t ->
+      Printf.printf "(%s, %s) -> (%s, %s, %s)\n"
+        state t.read t.to_state t.write t.action
+    ) transitions
+  ) tm.transitions
